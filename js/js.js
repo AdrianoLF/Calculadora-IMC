64ae7685c1ae2319619acac2b7d2e4c8a30b2305
AdrianoLF <adrianolf2003@gmail.com>
2022-06-12 16:29:00 -0300

    atualização js

--- js/js.js
+++ js/js.js
@@ -7,22 +7,29 @@ function notRefresh() { //Função principal que ativa as outras embaixo
 
     let peso = captacaoDados.querySelector('.peso'); //captando value de peso
     peso.value = Number(peso.value);//transformando valor de peso de string para number
-    // console.log(peso.value);
+    if(peso.value === 'NaN'){//Se peso.value é NaN, imprimir {}
+        document.getElementById('resultado').style.display = 'flex';
+        document.getElementById('resultado').innerHTML = `Peso inválido`;
+        return;
+    }
 
     let altura = captacaoDados.querySelector('.altura');//captando value de altura
     altura.value = Number(altura.value);//transformando valor de altura de string para number
-    // console.log(altura.value)
+    if(altura.value === 'NaN'){//Se altura.value é NaN, imprimir {}
+        document.getElementById('resultado').style.display = 'flex';
+        document.getElementById('resultado').innerHTML = ` Altura inválida`;
+        return;
+    }
 
-    let imc = peso.value / ((altura.value / 100) ** 2);//criando var imc e dando valor da formula do IMC
+    let imc = peso.value / ((altura.value / 100) ** 2);//criando var imc e seu valor é a formula do IMC (dividi altura/100 porque o input está pedindo em cm, e a formula é em metros)
     imc = imc.toFixed(2);//limitando casas decimais
-    console.log(imc)
 
-    if (imc < 18.5){
-        let quadro = "Abaixo do peso";
-        document.getElementById('resultado').style.display ='flex';
-        document.getElementById('resultado').style.backgroundColor = 'dodgerblue';
-        document.getElementById('resultadoImc').innerHTML = ` ${imc}`; 
-        document.getElementById('resultadoQuadro').innerHTML = ` ${quadro}`
+    if (imc < 18.5){//Se 'imc' menor que 18.5, executar {...}
+        let quadro = "Abaixo do peso";//var quadro = abaixo do peso
+        document.getElementById('resultado').style.display ='flex';//Mudando display do 'resultado', vir de 'none' para 'flex'
+        document.getElementById('resultado').style.backgroundColor = 'dodgerblue';//background de 'resultado' se altera
+        document.getElementById('resultadoImc').innerHTML = ` ${imc}`;//substituir o id "resultadoImc" pela var 'imc'
+        document.getElementById('resultadoQuadro').innerHTML = ` ${quadro}`//mudar a id "resultadoQuadro" pela var 'quadro'
 
     } else if (imc >= 18.5 && imc < 24.9){
         let quadro = "Peso normal";
@@ -54,8 +61,4 @@ function notRefresh() { //Função principal que ativa as outras embaixo
         document.getElementById('resultado').style.backgroundColor = 'maroon';
         document.getElementById('resultadoImc').innerHTML = ` ${imc}`; 
         document.getElementById('resultadoQuadro').innerHTML = ` ${quadro}`
-    }
-
-
-
-}
+    }}
